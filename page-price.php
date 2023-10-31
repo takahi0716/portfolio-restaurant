8c937dc0118f2ec783de01a6ddd60426ce4cfcff
oshima <oshima11223344@gmail.com>
2023-10-31 18:33:31 +0900

    添削後の修正

--- page-price.php
+++ page-price.php
@@ -5,7 +5,8 @@
   <section class="sub-mv">
     <picture class="sub-mv__image">
 
-      <source srcset="<?php echo get_theme_file_uri(); ?>/dist/assets/images/common/price-sp.jpg" media="(max-width: 767px)" />
+      <source srcset="<?php echo get_theme_file_uri(); ?>/dist/assets/images/common/price-sp.jpg"
+        media="(max-width: 767px)" />
 
       <img src="<?php echo get_theme_file_uri(); ?>/dist/assets/images/common/price-pc.jpg" alt="ダイバーの写真">
     </picture>
@@ -23,66 +24,66 @@
       <div class="page-price__column one-column">
         <?php $dinner_group = SCF::get_option_meta('price_option', 'dinner');
         if ($dinner_group[0]['dinner_course_name']) : ?>
-          <div class="page-price__container data-list data-list--sub" id="dinner">
-            <h2 class=" data-list__title"><span>ディナー</span></h2>
-            <dl class="data-list__data">
-              <?php foreach ($dinner_group as $fields) :
+        <div class="page-price__container data-list data-list--sub" id="dinner">
+          <h2 class=" data-list__title"><span>ディナー</span></h2>
+          <dl class="data-list__data">
+            <?php foreach ($dinner_group as $fields) :
                 if ($fields['dinner_course_name'] !== "" and $fields['dinner_course_price'] !== "") :
               ?>
-                  <dt><?php echo $fields['dinner_course_name']; ?></dt>
-                  <dd><?php echo $fields['dinner_course_price']; ?></dd>
-              <?php endif;
+            <dt><?php echo $fields['dinner_course_name']; ?></dt>
+            <dd><?php echo $fields['dinner_course_price']; ?></dd>
+            <?php endif;
               endforeach; ?>
-            </dl>
-          </div>
-        <?php endif; ?>
-
-        <?php $breakfast_group = SCF::get_option_meta('price_option', 'breakfast');
-        if ($breakfast_group[0]['breakfast_course_name']) : ?>
-          <div class="page-price__container data-list data-list--sub" id="breakfast">
-            <h2 class="data-list__title"><span>ランチ</span></h2>
-            <dl class="data-list__data">
-              <?php foreach ($breakfast_group as $fields) :
-                if ($fields['breakfast_course_name'] !== "" and $fields['breakfast_course_price'] !== "") :
-              ?>
-                  <dt><?php echo $fields['breakfast_course_name']; ?></dt>
-                  <dd><?php echo $fields['breakfast_course_price']; ?></dd>
-              <?php endif;
-              endforeach; ?>
-            </dl>
-          </div>
+          </dl>
+        </div>
         <?php endif; ?>
 
         <?php $launch_group = SCF::get_option_meta('price_option', 'launch');
         if ($launch_group[0]['launch_course_name']) : ?>
-          <div class="page-price__container data-list data-list--sub" id="launch">
-            <h2 class="data-list__title"><span>ブレックファースト</span></h2>
-            <dl class="data-list__data">
-              <?php foreach ($launch_group as $fields) :
+        <div class="page-price__container data-list data-list--sub" id="launch">
+          <h2 class="data-list__title"><span>ランチ</span></h2>
+          <dl class="data-list__data">
+            <?php foreach ($launch_group as $fields) :
                 if ($fields['launch_course_name'] !== "" and $fields['launch_course_price'] !== "") :
               ?>
-                  <dt><?php echo $fields['launch_course_name']; ?></dt>
-                  <dd><?php echo $fields['launch_course_price']; ?></dd>
-              <?php endif;
+            <dt><?php echo $fields['launch_course_name']; ?></dt>
+            <dd><?php echo $fields['launch_course_price']; ?></dd>
+            <?php endif;
               endforeach; ?>
-            </dl>
-          </div>
+          </dl>
+        </div>
+        <?php endif; ?>
+
+        <?php $breakfast_group = SCF::get_option_meta('price_option', 'breakfast');
+        if ($breakfast_group[0]['breakfast_course_name']) : ?>
+        <div class="page-price__container data-list data-list--sub" id="breakfast">
+          <h2 class="data-list__title"><span>ブレックファースト</span></h2>
+          <dl class="data-list__data">
+            <?php foreach ($breakfast_group as $fields) :
+                if ($fields['breakfast_course_name'] !== "" and $fields['breakfast_course_price'] !== "") :
+              ?>
+            <dt><?php echo $fields['breakfast_course_name']; ?></dt>
+            <dd><?php echo $fields['breakfast_course_price']; ?></dd>
+            <?php endif;
+              endforeach; ?>
+          </dl>
+        </div>
         <?php endif; ?>
 
         <?php $special_group = SCF::get_option_meta('price_option', 'special');
         if ($special_group[0]['special_course_name']) : ?>
-          <div class="page-price__container data-list data-list--sub" id="special">
-            <h2 class=" data-list__title"><span>スペシャルコース</span></h2>
-            <dl class="data-list__data">
-              <?php foreach ($special_group as $fields) :
+        <div class="page-price__container data-list data-list--sub" id="special">
+          <h2 class=" data-list__title"><span>スペシャルコース</span></h2>
+          <dl class="data-list__data">
+            <?php foreach ($special_group as $fields) :
                 if ($fields['special_course_name'] !== "" and $fields['special_course_price'] !== "") :
               ?>
-                  <dt><?php echo $fields['special_course_name']; ?></dt>
-                  <dd><?php echo $fields['special_course_price']; ?></dd>
-              <?php endif;
+            <dt><?php echo $fields['special_course_name']; ?></dt>
+            <dd><?php echo $fields['special_course_price']; ?></dd>
+            <?php endif;
               endforeach; ?>
-            </dl>
-          </div>
+          </dl>
+        </div>
         <?php endif; ?>
       </div>
 
